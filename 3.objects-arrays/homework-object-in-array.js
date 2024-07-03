f32417504356c16cc72f836f37781c7091124863
ralucastr <129978480+ralucastr@users.noreply.github.com>
2024-07-03 15:20:42 +0300

    added homework

---
+++ 3.objects-arrays/homework-object-in-array.js
@@ -0,0 +1,17 @@
+let library = {
+    name: "City Library",
+    books: [
+        { title: "1984", author: "George Orwell" },
+        { title: "Brave New World", author: "Aldous Huxley" },
+        { title: "Gone With The Wind", author: "Margaret Mitchell" }
+    ]
+};
+
+console.log("First Book Object:", library.books[0]);
+
+// Store the 1st book in a variable
+let firstBook = library.books[0];
+
+// Print the whole book object
+console.log("First Book Object:", firstBook);
+
--- 3.objects-arrays/homework.js
+++ 3.objects-arrays/homework.js
@@ -42,3 +42,60 @@ console.log(`${zoo[2].name} has been fed. Remaining ${zoo[2].foodType} stock: ${
 console.log(`${zoo[0].name} now has ${zoo[0].foodStock} units of ${zoo[0].foodType}.`);
 console.log(`${zoo[1].name} now has ${zoo[1].foodStock} units of ${zoo[1].foodType}.`);
 console.log(`${zoo[2].name} now has ${zoo[2].foodStock} units of ${zoo[2].foodType}.`);
+
+//---------------------------------------------------------------------------------------------------------------------//
+
+// // Creating a book object
+// let book = {
+//     title: "The Great Gatsby",
+//     author: "F. Scott Fitzgerald",
+//     pages: 180
+// };
+
+// // Modify the properties
+// book.title = "The Great Gatsby - Updated Edition";
+// book.pages = 200;
+
+// // Print the modified book properties
+// console.log("Book:");
+// console.log(`Title: ${book.title}`);
+// console.log(`Author: ${book.author}`);
+// console.log(`Pages: ${book.pages}`);
+
+// Creating a library object
+let library = {
+    name: "City Library",
+    books: [
+        { title: "1984", author: "George Orwell" },
+        { title: "To Kill a Mockingbird", author: "Harper Lee" },
+        { title: "Moby-Dick", author: "Herman Melville" }
+    ]
+};
+
+// // Update the book list
+// library.books.push({ title: "The Catcher in the Rye", author: "J.D. Salinger" });
+
+// // Print the library information
+// console.log("\nLibrary:");
+// console.log(`Name: ${library.name}`);
+// console.log("Books:");
+// library.books.forEach((book, index) => {
+//     console.log(`${index + 1}. ${book.title} by ${book.author}`);
+// });
+
+// Create an object representing John with a list of books he has read
+let john = {
+    name: "John Doe",
+    readBooks: [
+        { title: "The Hobbit", author: "J.R.R. Tolkien" },
+        { title: "The Catcher in the Rye", author: "J.D. Salinger" },
+        { title: "Pride and Prejudice", author: "Jane Austen" }
+    ]
+};
+
+// // Access and print the 2nd book read by John
+// let secondBookReadByJohn = john.readBooks[1];
+// console.log("\nJohn's 2nd Book:");
+// console.log(`Title: ${secondBookReadByJohn.title}`);
+// console.log(`Author: ${secondBookReadByJohn.author}`);
+
