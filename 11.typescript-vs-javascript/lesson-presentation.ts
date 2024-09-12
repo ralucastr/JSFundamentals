72c6f2b3760bf9ba24aad527baf0d2ae4df64bdd
Gabriel Vladulescu <gabriel.vladulescu@qubiz.com>
2024-09-13 02:36:49 +0300

    Update last lesson

--- 11.typescript-vs-javascript/lesson-presentation.ts
+++ 11.typescript-vs-javascript/lesson-presentation.ts
@@ -1,3 +1,46 @@
+// example 1
+// let message = "Hello, World!";
+// console.log(message); // Output: "Hello, World!"
+
+// message = 42;  // No type error, now the variable is a number
+// console.log(message); // Output: 42
+
+let message: string = "Hello, TypeScript!";
+console.log(message); // Output: "Hello, TypeScript!"
+
+// This will cause a compile-time error in TypeScript
+// message = 42; // Error: Type 'number' is not assignable to type 'string'
+
+
+// example 2
+// Typescript Variant
+interface User {
+    name: string;
+    age: number;
+}
+
+function greet(user: User): string {
+    return `Hello, ${user.name}. You are ${user.age} years old.`;
+}
+
+let user: User = {
+    name: "Alice",
+    age: 25 
+};
+console.log(greet(user)); // Output: Hello, Alice. You are 25 years old.
+
+// Javascript variant
+// function greet(user) {
+//     return `Hello, ${user.name}. You are ${user.age} years old.`;
+// }
+  
+// let user = { name: "Alice", age: 25 };
+// console.log(greet(user)); // Output: Hello, Alice. You are 25 years old.
+
+
+
+// Example 3
+// Typescript
 class Person {
     name: string;
     age: number;
